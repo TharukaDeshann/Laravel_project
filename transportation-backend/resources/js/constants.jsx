dcdfefc4d608c7b5183e310d774f1a6b1ea15538
Your Name <your.email@example.com>
2024-12-14 17:18:43 +0530

    before adding assigned user to vehicles

---
+++ transportation-backend/resources/js/constants.jsx
@@ -0,0 +1,9 @@
+export const VEHICLE_STATUS_CLASS_MAP = {
+    Inactive: "bg-amber-500",
+    Active: "bg-blue-500",
+  };
+export const VEHICLE_STATUS_TEXT_MAP = {
+    Inactive: "Inactive",
+    Active: "Active",
+  };
+  
